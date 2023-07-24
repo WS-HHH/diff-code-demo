ac1b29da143c26a8567faa1405baac329bc81baa
sswang <1395363307@qq.com>
2023-07-25 07:28:25 +0800

    feat:add diff code

---
+++ src/components/DiffCode/const.js
@@ -0,0 +1,66 @@
+export const STUDENTS = [
+  {"students": [{"name": "Alice","age": 18,"grades": {"math": 95,"english": 87,"history": 92}},{
+        "name": "Bob",
+        "age": 17,
+        "grades": {
+          "math": 88,
+          "english": 92,
+          "history": 78
+        }
+      },
+      {
+        "name": "Charlie",
+        "age": 19,
+        "grades": {
+          "math": 90,
+          "english": 85,
+          "history": 89
+        }
+      },
+      {
+        "name": "David",
+        "age": 16,
+        "grades": {
+          "math": 78,
+          "english": 85,
+          "history": 82
+        }
+      }
+      // 继续添加更多的学生数据...
+    ],
+    "teachers": [
+      {
+        "name": "Mr. Smith",
+        "subject": "Mathematics",
+        "experience": 15
+      },
+      {
+        "name": "Ms. Johnson",
+        "subject": "English",
+        "experience": 12
+      },
+      {
+        "name": "Mrs. Lee",
+        "subject": "History",
+        "experience": 20
+      }
+      // 继续添加更多的教师数据...
+    ],
+    "courses": [
+      {
+        "name": "Mathematics",
+        "description": "Learn about algebra, geometry, and calculus."
+      },
+      {
+        "name": "English",
+        "description": "Improve your reading, writing, and communication skills."
+      },
+      {
+        "name": "History",
+        "description": "Explore the events and people that shaped the past."
+      }
+      // 继续添加更多的课程数据...
+    ]
+    // 可以继续添加更多的数据...
+  }
+];
---
+++ src/components/DiffCode/index.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue';
+import { STUDENTS } from './const';
+
+const code = ref(STUDENTS[0]);
+
+// function increment() {
+//   count.value++
+// }
+  // defineProps({
+  //   msg: {
+  //     type: String,
+  //     required: true
+  //   }
+  // })
+</script>
+
+<template>
+  <div class="container">
+    <pre>
+      <code>{{ code }}</code>
+    </pre>
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+  background-color: azure;
+  color: black;
+  /* box-shadow: ; */
+}
+/* h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+} */
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
